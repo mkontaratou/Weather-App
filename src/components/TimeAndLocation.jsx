953fc5d6feca04d55c931b0a21e87e1e0c7ceff1
mkontaratou <kontaratou.mar@gmail.com>
2023-03-03 00:03:53 +0200

    final commits

--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+import { formatToLocalTime } from "../services/weatherServices";
 
-function TimeAndLocation() {
+function TimeAndLocation({ weather: { dt, timezone, name, country } }) {
   return (
     <div>
         <div className='flex items-center justify-center my-6'>
             <p className='text-white text-xl font-extralight'>
-                Sunday, 26 February 2023 | Local time: 6:48 PM
+                {formatToLocalTime(dt, timezone)}
             </p>
         </div>
 
         <div className='flex items-center justify-center my-3'>
             <p className='text-white text-3xl font-medium'>
-                Berlin, DE
+            {`${name}, ${country}`}
             </p>
         </div>
         
